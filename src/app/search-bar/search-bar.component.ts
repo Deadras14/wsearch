b6bce46e3539e331adf4c257de3d4c0b00d8aab4
ralph29 <ralphalmarez29@gmail.com>
2021-07-02 12:26:58 +0800

    Component Design Methodology

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  term = '';
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
