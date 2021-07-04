6b6c50d2f4d0bf0e20bae07ca80206f0c33738d8
ralph29 <ralphalmarez29@gmail.com>
2021-07-04 13:25:15 +0800

    Seeing the Request Response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { PageListComponent } from './page-list/page-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { PageListComponent } from './page-list/page-list.component';
     PageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
